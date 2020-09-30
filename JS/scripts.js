f0f197bc2a2c5fdae3eb761a8339c77e1a7b6e7e
Raritoken <0956540@hr.nl>
2020-09-30 21:06:44 +0200

    Added back to top button with animation, optimized for both mobile and pc

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,3 +8,18 @@ $(function() {
         window.open("https://tekken.fandom.com/wiki/Kazumi_Mishima");
     });
 });
+$(function() {
+    $(window).scroll(function() {
+        var showBTN = 1000;
+        if ($(this).scrollTop() > showBTN) {
+            $("#btt").fadeIn();
+        }
+        else {
+            $("#btt").fadeOut();
+        }
+    });
+    $("#btt").click(function() {
+        $('html, body').animate({scrollTop: 0}, 500);
+        return false;
+    })
+});
